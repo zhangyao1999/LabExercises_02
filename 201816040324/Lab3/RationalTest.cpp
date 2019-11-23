9676cb8973a510638d3de1e5034b3792bb9b3fe1
zhangyao1999 <56502471+zhangyao1999@users.noreply.github.com>
2019-11-23 23:13:20 +0800

    Create RationalTest.cpp

---
+++ 201816040324/Lab3/RationalTest.cpp
@@ -0,0 +1,76 @@
+// Lab 3: RationalTest.cpp
+// RationalNumber test program.
+#include <iostream>
+using namespace std;
+
+#include "RationalNumber.h"
+
+int main()
+{
+   RationalNumber c( 7, 0), d( 3, 9 ), x;
+
+   c.printRational();
+   cout << " + ";
+   d.printRational();
+   cout << " = ";
+   x = c + d; // test overloaded operators + and =
+   x.printRational();
+
+   cout << '\n';
+   c.printRational();
+   cout << " - ";
+   d.printRational();
+   cout << " = ";
+   x = c - d; // test overloaded operators - and =
+   x.printRational();
+
+   cout << '\n';
+   c.printRational();
+   cout << " * ";
+   d.printRational();
+   cout << " = ";
+   x = c * d; // test overloaded operators * and =
+   x.printRational();
+
+   cout << '\n';
+   c.printRational();
+   cout << " / ";
+   d.printRational();
+   cout << " = ";
+   x = c / d; // test overloaded operators / and =
+   x.printRational();
+
+   cout << '\n';
+   c.printRational();
+   cout << " is:\n";
+
+   // test overloaded greater than operator
+   cout << ( ( c > d ) ? "  > " : "  <= " );
+   d.printRational();
+   cout << " according to the overloaded > operator\n";
+
+   // test overloaded less than operator
+   cout << ( ( c < d ) ? "  < " : "  >= " );
+   d.printRational();
+   cout << " according to the overloaded < operator\n";
+
+   // test overloaded greater than or equal to operator
+   cout << ( ( c >= d ) ? "  >= " : "  < " );
+   d.printRational();
+   cout << " according to the overloaded >= operator\n";
+
+   // test overloaded less than or equal to operator
+   cout << ( ( c <= d ) ? "  <= " : "  > " );
+   d.printRational();
+   cout << " according to the overloaded <= operator\n";
+
+   // test overloaded equality operator
+   cout << ( ( c == d ) ? "  == " : "  != " );
+   d.printRational();
+   cout << " according to the overloaded == operator\n";
+
+   // test overloaded inequality operator
+   cout << ( ( c != d ) ? "  != " : "  == " );
+   d.printRational();
+   cout << " according to the overloaded != operator" << endl;
+} // end main
