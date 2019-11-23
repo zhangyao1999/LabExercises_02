15389af3b1c3cb590725d919e1b07b08030429a6
zhangyao1999 <56502471+zhangyao1999@users.noreply.github.com>
2019-11-23 23:11:45 +0800

    Create HugeIntTest.cpp

---
+++ 201816040324/Lab2/HugeIntTest.cpp
@@ -0,0 +1,50 @@
+// Lab 2: HugeIntTest.cpp
+// HugeInt test program.
+#include <iostream>
+using namespace std;
+
+#include "Hugeint.h"
+
+int main()
+{
+   HugeInt n1( 7654321 );
+   HugeInt n2( 7891234 );
+   HugeInt n3( "99999999999999999999999999999" );
+   HugeInt n4( "1" );
+   HugeInt result;
+
+   cout << "n1 is " << n1 << "\nn2 is " << n2
+      << "\nn3 is " << n3 << "\nn4 is " << n4
+      << "\nresult is " << result << "\n\n";
+
+   // test relational and equality operators
+   if ( n1 == n2 )
+      cout << "n1 equals n2" << endl;
+
+   if ( n1 != n2 )
+      cout << "n1 is not equal to n2" << endl;
+
+   if ( n1 < n2 )
+      cout << "n1 is less than n2" << endl;
+
+   if ( n1 <= n2 )
+      cout << "n1 is less than or equal to n2" << endl;
+
+   if ( n1 > n2 )
+      cout << "n1 is greater than n2" << endl;
+
+   if ( n1 >= n2 )
+      cout << "n1 is greater than or equal to n2" << endl;
+
+   result = n1 + n2;
+   cout << n1 << " + " << n2 << " = " << result << "\n\n";
+
+   cout << n3 << " + " << n4 << "\n= " << ( n3 + n4 ) << "\n\n";
+
+   result = n1 + 9;
+   cout << n1 << " + " << 9 << " = " << result << endl;
+
+   result = n2 + "10000";
+   cout << n2 << " + " << "10000" << " = " << result << endl;
+
+} // end main
